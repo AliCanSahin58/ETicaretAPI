026ddad0c15783fd459f8994584f4e7001e2aeb8
Ali Can Sahin <alicanshn180@gmail.com>
2023-01-24 02:06:04 +0300

    Add project files.

---
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productServices;
+
+        public ProductsController(IProductService productServices)
+        {
+            _productServices = productServices;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+           var products = _productServices.GetProducts();
+            return Ok(products);
+        }
+
+            
+    }
+}
